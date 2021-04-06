0ecdd30aeda5065aae177f7ca9d25388dbf89e42
HoegaasianCode <80755360+HoegaasianCode@users.noreply.github.com>
2021-04-06 14:39:58 +0200

    Update Program.cs

--- MusicalNoteRange/Program.cs
+++ MusicalNoteRange/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace MusicalNoteRange
 {
@@ -7,19 +7,27 @@ namespace MusicalNoteRange
         // https://edabit.com/challenge/Z7bgqN8vK5gqrQpqL
         // SOLVED
 
+        // Example: Instrument with range of D4-C7 ->
+        // At the lowest octave, A4, B4, C4 are impossible.
+        // Can play any other note within the 4th octave (D4,E4,F4,G4).
+        // A4, B4 and C4 are all greater values than D4, thus:
+        // if(Octave == Instrument.LowestOctave && Note > Instrument.LowestNote) IsValid = false;
+        // Vice versa for notes on the highest possible octaves. 
+
         static void Main(string[] args)
         {
-            Instrument piccolo = new("Piccolo", new short[] { 4, 7 }, new char[] { 'D', 'C' });
-            Instrument tuba = new("Tuba", new short[] { 1, 4 }, new char[] { 'D', 'F' });
-            Instrument guitar = new("Guitar", new short[] { 3, 6 }, new char[] { 'E', 'E' });
-            Instrument piano = new("Piano", new short[] { 0, 8 }, new char[] { 'A', 'C' });
-            Instrument violin = new("Violin", new short[] { 3, 7}, new char[] { 'G', 'A' });
-            NoteValidation noteValidation = new("Piccolo", "A3", piccolo, tuba, guitar, piano, violin);
+            Instrument piccolo = new("Piccolo", new int[] { 4, 7 }, new char[] { 'D', 'C' });
+            Instrument tuba = new("Tuba", new int[] { 1, 4 }, new char[] { 'D', 'F' });
+            Instrument guitar = new("Guitar", new int[] { 3, 6 }, new char[] { 'E', 'E' });
+            Instrument piano = new("Piano", new int[] { 0, 8 }, new char[] { 'A', 'C' });
+            Instrument violin = new("Violin", new int[] { 3, 7 }, new char[] { 'G', 'A' });
+            NoteValidation noteValidation = new("Piccolo", "D4", piccolo, tuba, guitar, piano, violin);
             noteValidation.LookUpInstrument();
-            noteValidation.IsValidOctave();
-            noteValidation.IsValidNote();
-            bool isValid = noteValidation.IsValidNoteAndOctave();
-            Console.Write(isValid);
+            noteValidation.SetOctaveToInt();
+            noteValidation.IsValidOctalRange();
+            noteValidation.IsValidLowerNote();
+            noteValidation.IsValidHigherNote();
+            Console.Write(noteValidation.IsValidNoteAndOctave());
         }
     }
 }
